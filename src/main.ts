69004b2c52e272886c255b5009021feed4b022b7
Jaggernaut555 <jay_jag6@hotmail.com>
2022-02-26 01:35:58 -0800

    initial release

---
+++ src/main.ts
@@ -0,0 +1,38 @@
+import { ButtonType } from "midi-mixer-plugin";
+import axios from "axios";
+
+let settings: Record<string, any>;
+let url: string;
+let payload: string;
+
+const sendButton = new ButtonType("SendButton", {
+  name: "Send Webhook",
+  active: true,
+});
+
+sendButton.on("pressed", async () => {
+  console.log("pressed");
+  await axios.post(url, JSON.parse(payload))
+  .then((response: any) => {
+    console.log(response);
+    console.log("Success");
+  })
+  .catch((err) => {
+    console.log(err);
+    console.log("Error");
+  })
+});
+
+async function init() {
+
+  settings = await $MM.getSettings();
+
+  url = settings["WebhookURL"];
+  payload = settings["WebhookPayload"];
+
+  if (!payload) {
+    payload = "{ }";
+  }
+}
+
+init();
